3bf849a27e7eb9fcec9fc76e9866c282c2b77e75
Sarcolytic <orroreuomo@gmail.com>
2019-04-22 09:19:53 +0300

    add game name to main menu

--- src/view/MainMenuView.ts
+++ src/view/MainMenuView.ts
@@ -7,9 +7,17 @@ export default class MainMenuView extends PIXI.Container {
 	constructor() {
 		super();
 
+		const gameName = new PIXI.extras.BitmapText('MINESWEEPER', { font: '50px LibelSuit', tint: 0x000000 });
+		gameName.anchor = 0.5;
+		gameName.position.set(GameConstants.GAME_CENTER_X, 120);
+
 		const playButton = new Button('PLAY');
 		playButton.on('click', () => { this.emit(MainMenuView.EVENT_BUTTON_CLICK); });
 		playButton.position.set(GameConstants.GAME_CENTER_X, GameConstants.GAME_CENTER_Y);
-		this.addChild(playButton);
+
+		this.addChild(
+			gameName,
+			playButton as PIXI.DisplayObject,
+		);
 	}
 }
